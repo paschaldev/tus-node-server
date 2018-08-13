011357cf919f7e71d444ca0a3b115110cc8aa1ad
Ezeugwu Paschal <ezeugwupaschal@gmail.com>
2018-08-13 08:09:02 +0100

    Initial Commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Server = require('./lib/Server');
 const DataStore = require('./lib/stores/DataStore');
 const FileStore = require('./lib/stores/FileStore');
 const GCSDataStore = require('./lib/stores/GCSDataStore');
+const CloudinaryStore = require('./lib/stores/CloudinaryStore');
 const S3Store = require('./lib/stores/S3Store');
 const ERRORS = require('./lib/constants').ERRORS;
 const EVENTS = require('./lib/constants').EVENTS;
@@ -13,6 +14,7 @@ module.exports = {
     DataStore,
     FileStore,
     GCSDataStore,
+    CloudinaryStore,
     S3Store,
     ERRORS,
     EVENTS,
